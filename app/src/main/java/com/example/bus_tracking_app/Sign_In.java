8c7d449361b58907d833c1b30394cef5d4d93fbb
shivam-sharma-2000 <88277shiv@gmail.com>
2022-03-30 11:49:58 +0530

    New user button

--- app/src/main/java/com/example/bus_tracking_app/Sign_In.java
+++ app/src/main/java/com/example/bus_tracking_app/Sign_In.java
@@ -5,17 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Sign_In extends AppCompatActivity {
 
+    TextView SingInToSignUp;
+    Button SignIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_page);
+        setContentView(R.layout.activity_sign);
+
+        SingInToSignUp = findViewById(R.id.SignInToSignUp);
+        SignIn= findViewById(R.id.SignIn);
+
+        SignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Home_Page.class);
+                startActivity(intent);
+            }
+        });
+
+        SingInToSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Sign_Up.class);
+                startActivity(intent);
+            }
+        });
     }
     public void changeActivity(View view){
-        Intent intent = new Intent(this, Sign_Up.class);
-        startActivity(intent);
+
     }
 
 }
